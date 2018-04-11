0b4567fbd56a6f9ee775ecb14d20f2b8a842b1e9
odh621 <odh621@naver.com>
2018-04-11 13:13:57 +0900

    first commit

---
+++ boot/src/test/java/com/donghun/BootApplicationTests.java
@@ -0,0 +1,33 @@
+package com.donghun;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.donghun.Repository.BoardRepository;
+import com.donghun.models.Board;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BootApplicationTests {
+	@Autowired
+	BoardRepository boardRepository;
+	
+	@Test
+	public void testInsert200() {
+		for(int i = 1; i<=200; i++) {
+			Board board = new Board();
+			board.setTitle("제목.." + i);
+			board.setContent("내용.." + i);
+			board.setWriter("user0"+(i%20));
+			boardRepository.save(board);
+		}
+	}
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
